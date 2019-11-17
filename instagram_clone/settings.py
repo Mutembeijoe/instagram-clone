d5d13c4d76123674edb2a534daca91e0e2306f47
mutembeijoe <mutembeijoe@gmail.com>
2019-11-17 15:36:17 +0300

    configures media files to be uploaded to the Media root file

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -120,7 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
--- instagram_clone/urls.py
+++ instagram_clone/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 # from django.contrib.auth.urls
 
 urlpatterns = [
@@ -24,3 +26,6 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
