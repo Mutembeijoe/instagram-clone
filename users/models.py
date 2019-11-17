55b1fbec3c1ae551b06c2a50264389c81081e92c
mutembeijoe <mutembeijoe@gmail.com>
2019-11-17 14:21:22 +0300

    adds a image field for user avatar to the Custom user model

--- users/models.py
+++ users/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    pass
+    avatar = models.ImageField(upload_to='avatar_gallery', default='default.jpg')
