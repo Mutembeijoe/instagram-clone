14ad10670675b027b9356a36298a24b18ae46b10
mutembeijoe <mutembeijoe@gmail.com>
2019-11-17 17:17:02 +0300

    adds a Comment model

--- posts/models.py
+++ posts/models.py
@@ -13,3 +13,10 @@ class Image(models.Model):
         return self.name
         
 
+class Comment(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name='comments')
+    comment = models.CharField(max_length=140)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comment
