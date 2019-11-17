1bdf06413534a7a6cdc4070cdaa8097c8161ce91
mutembeijoe <mutembeijoe@gmail.com>
2019-11-17 14:19:15 +0300

    creates a class based view and configures url for user registration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+# from .models import CustomUser
+from .forms import CustomUserCreationForm
 # Create your views here.
+
+class CreateUserView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy('login')
