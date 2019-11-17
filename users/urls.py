03d66ecc2f365567baab0f3cbac68fbe5490e3ef
mutembeijoe <mutembeijoe@gmail.com>
2019-11-17 21:10:49 +0300

    configures settings to enale the project to load static files from a project level static file

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateUserView
+from .views import CreateUserView, DetailUserView
 
 
 urlpatterns = [
-    path('register/', CreateUserView.as_view(), name="register")
+    path('register/', CreateUserView.as_view(), name="register"),
+    path('profile/<int:pk>/', DetailUserView.as_view(), name='profile')
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
-# from .models import CustomUser
+from .models import CustomUser
 from .forms import CustomUserCreationForm
 # Create your views here.
 
@@ -9,3 +10,7 @@ class CreateUserView(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'signup.html'
     success_url = reverse_lazy('login')
+
+class DetailUserView(DetailView):
+    model = CustomUser
+    template_name = 'profile.html'
