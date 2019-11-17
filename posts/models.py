747f364fa58b73ab6f2d7e637f61e3a0faa11f63
mutembeijoe <mutembeijoe@gmail.com>
2019-11-17 16:20:58 +0300

    adds image model to posts

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='posts_gallery')
+    name = models.CharField(max_length=100)
+    caption = models.CharField(max_length=200)
+    user = models.ForeignKey(get_user_model(), on_delete = models.CASCADE, related_name='images')
+    comment = models.CharField(max_length=250)
+
