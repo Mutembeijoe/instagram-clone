39720535728a95020630ba80a6fc3bc394cfcc09
mutembeijoe <mutembeijoe@gmail.com>
2019-11-18 07:15:08 +0300

    deletes a joe's profile

--- users/models.py
+++ users/models.py
@@ -5,16 +5,18 @@ from django.contrib.auth import get_user_model
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    pass
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete = models.CASCADE, related_name='avatar')
     avatar = models.ImageField(upload_to='avatar_gallery', default='default.jpg')
+    telephone = models.CharField(max_length=10)
+    
 
 
-    def __str__(self):
-        return f"{self.user.username}'s profile" 
+# class Profile(models.Model):
+#     user = models.OneToOneField(CustomUser, on_delete = models.CASCADE, related_name='avatar')
+#     avatar = models.ImageField(upload_to='avatar_gallery', default='default.jpg')
+
+
+#     def __str__(self):
+#         return f"{self.user.username}'s profile" 
 
 
 # class Following(models.Model):
