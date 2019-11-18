28d331d3bb680e1abc91bd300079aee130405680
mutembeijoe <mutembeijoe@gmail.com>
2019-11-18 13:03:09 +0300

    adds tests for users

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class PostTest(TestCase):
+   
+    def test_list_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 302)
+        #Rdirect expected due to login
+        self.assertTemplateUsed('home.html')
