9b5e48850b72e13ef7a6ccf0a6a64f802b0e0ece
mutembeijoe <mutembeijoe@gmail.com>
2019-11-18 07:14:07 +0300

    adds a  url route and view to display user update form

--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model
 from .models import CustomUser
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm,CustomUserChangeForm
 # Create your views here.
 
 class CreateUserView(CreateView):
@@ -15,3 +16,11 @@ class DetailUserView(DetailView):
     model = CustomUser
     template_name = 'profile.html'
     context_object_name = 'user'
+
+class UpdateUserView(UpdateView):
+    # form_class = CustomUserChangeForm
+    # template_name = 'update_user.html'
+    # success_url = reverse_lazy('profile')
+    model = get_user_model()
+    template_name = 'update_user.html'
+    fields = ['username', 'email', 'avatar']
